a82d6706a022021a557176f1161f6a3ba10d910b
Germán Martín <gmag11@gmail.com>
2016-08-19 13:30:01 +0200

    Event handler fix

--- examples/NTPClient/ESPNTPClient.cpp
+++ examples/NTPClient/ESPNTPClient.cpp
@@ -94,10 +94,14 @@ time_t NTPClient::getTime()
 			_firstSync = secsSince1970;
 			DEBUGLOG("First sync! %s\r\n", getTimeDateString(getFirstSync()).c_str());
 		}
+		if (onSyncEvent != NULL)
+			onSyncEvent(timeSyncd);     // call the handler
 		DEBUGLOG("Succeccful NTP sync at %s\r\n", getTimeDateString(getLastNTPSync()).c_str());
 	}
 	else {
 		DEBUGLOG("-- NTP error :-(\r\n");
+		if (onSyncEvent != NULL)
+			onSyncEvent(noResponse);     // call the handler
 		setSyncInterval(getShortInterval()); // Fast refresh frequency, until successful sync
 	}
 
