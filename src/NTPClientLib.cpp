cf7d80ae6a96f4ba18558fc48e09c4901b12e74c
Germán Martín <gmag11@gmail.com>
2018-03-14 08:56:46 +0100

    Clean compiler warnings

--- src/NTPClientLib.cpp
+++ src/NTPClientLib.cpp
@@ -370,7 +370,7 @@ bool NTPClient::summertime (int year, byte month, byte day, byte hour, byte tzHo
 {
     if ((month < 3) || (month > 10)) return false; // keine Sommerzeit in Jan, Feb, Nov, Dez
     if ((month > 3) && (month < 10)) return true; // Sommerzeit in Apr, Mai, Jun, Jul, Aug, Sep
-    if (month == 3 && (hour + 24 * day) >= (1 + tzHours + 24 * (31 - (5 * year / 4 + 4) % 7)) || month == 10 && (hour + 24 * day) < (1 + tzHours + 24 * (31 - (5 * year / 4 + 1) % 7)))
+    if ((month == 3 && (hour + 24 * day) >= (1 + tzHours + 24 * (31 - (5 * year / 4 + 4) % 7))) || (month == 10 && (hour + 24 * day) < (1 + tzHours + 24 * (31 - (5 * year / 4 + 1) % 7))))
         return true;
     else
         return false;
